5dea1b8c8f38699a4d97cc4deef061479d85400b
Rachel Brindle and Wiley Kestner <rbrindle+wkestner@pivotal.io>
2015-01-28 15:50:35 -0800

    Initial Commit

---
+++ Ra/Ra.h
@@ -0,0 +1,19 @@
+//
+//  Ra.h
+//  Ra
+//
+//  Created by pivotal on 1/28/15.
+//  Copyright (c) 2015 Rachel Brindle. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Ra.
+FOUNDATION_EXPORT double RaVersionNumber;
+
+//! Project version string for Ra.
+FOUNDATION_EXPORT const unsigned char RaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Ra/PublicHeader.h>
+
+
