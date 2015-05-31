354bc90a3321fc5fe21798958bd76d87dc906a85
Rachel Brindle <you@subluminal.net>
2015-05-31 09:20:26 -0700

    Use private static struct for associated object injector key

--- Ra/NSObject+Ra.swift
+++ Ra/NSObject+Ra.swift
@@ -1,22 +1,16 @@
-//
-//  NSObject+Ra.swift
-//  Ra
-//
-//  Created by Rachel Brindle on 2/20/15.
-//  Copyright (c) 2015 Rachel Brindle. All rights reserved.
-//
-
 import Foundation
 
-var AssociatedObjectHandle = 0x12345678
-
 public extension NSObject {
+    private struct AssociatedKeys {
+        static var injectorKey = "ra_injectorKey"
+    }
+
     public internal(set) var injector : Injector? {
         get {
-            return objc_getAssociatedObject(self, &AssociatedObjectHandle) as? Injector
+            return objc_getAssociatedObject(self, &AssociatedKeys.injectorKey) as? Injector
         }
         set {
-            objc_setAssociatedObject(self, &AssociatedObjectHandle, newValue, objc_AssociationPolicy(OBJC_ASSOCIATION_ASSIGN))
+            objc_setAssociatedObject(self, &AssociatedKeys.injectorKey, newValue, objc_AssociationPolicy(OBJC_ASSOCIATION_ASSIGN))
         }
     }
 }
